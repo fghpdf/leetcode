561df4d96faf4f7a18817dcac39b32075f5d9aa7
fghpdf <quxiangxuanqxx@gmail.com>
2019-12-15 19:21:11 +0800

    feat: solved 208

---
+++ src/com/fghpdf/ImplementTrie/Solution.java
@@ -0,0 +1,56 @@
+package com.fghpdf.ImplementTrie;
+
+/**
+ * @author fghpdf
+ * @date 2019/12/15
+ * https://leetcode.com/problems/implement-trie-prefix-tree/
+ * TODO: 还需要看，状态不好
+ **/
+class TrieNode {
+	boolean isWord;
+	TrieNode[] children = new TrieNode[26];
+	TrieNode() {}
+}
+
+class Trie {
+	private TrieNode root;
+	public Trie() {
+		root = new TrieNode();
+	}
+
+	public void insert(String word) {
+		TrieNode ws = root;
+		for(int i = 0; i < word.length(); i++){
+			char c = word.charAt(i);
+			if(ws.children[c - 'a'] == null){
+				ws.children[c - 'a'] = new TrieNode();
+			}
+			ws = ws.children[c - 'a'];
+		}
+		ws.isWord = true;
+	}
+
+	public boolean search(String word) {
+		TrieNode ws = root;
+		for(int i = 0; i < word.length(); i++){
+			char c = word.charAt(i);
+			if(ws.children[c - 'a'] == null) {
+				return false;
+			}
+			ws = ws.children[c - 'a'];
+		}
+		return ws.isWord;
+	}
+
+	public boolean startsWith(String prefix) {
+		TrieNode ws = root;
+		for(int i = 0; i < prefix.length(); i++){
+			char c = prefix.charAt(i);
+			if(ws.children[c - 'a'] == null) {
+				return false;
+			}
+			ws = ws.children[c - 'a'];
+		}
+		return true;
+	}
+}
