5effbb7d3e21788b53b11f8e4b2a33fd26c7d9cd
瞿祥轩 <quxiangxuanqxx@hotmail.com>
2020-05-25 17:29:19 +0800

    feat: solved 518

---
+++ src/com/fghpdf/CoinChange2/Solution.java
@@ -0,0 +1,36 @@
+package com.fghpdf.CoinChange2;
+
+/**
+ * @author fghpdf
+ * @date 2020/5/25
+ * @link
+ **/
+public class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length + 1][amount + 1];
+
+        // base
+        for (int i = 0; i < amount + 1; i++) {
+            dp[0][i] = 0;
+        }
+        for (int i = 0; i < coins.length + 1; i++) {
+            dp[i][0] = 1;
+        }
+
+        // loop [1, coins]
+        for (int i = 1; i < coins.length + 1; i++) {
+            // loop [1, amount]
+            for (int j = 1; j < amount + 1; j++) {
+                // choose
+                if (j - coins[i-1] >= 0) {
+                    dp[i][j] = dp[i-1][j] +
+                            dp[i][j-coins[i-1]];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[coins.length][amount];
+    }
+}
--- src/com/fghpdf/PartitionEqualSubsetSum/Solution.java
+++ src/com/fghpdf/PartitionEqualSubsetSum/Solution.java
@@ -1,6 +1,8 @@
 package com.fghpdf.PartitionEqualSubsetSum;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author fghpdf
@@ -48,5 +50,67 @@ public class Solution {
         }
 
         return dp[nums.length][sum/2];
+    }
+
+    private static class Segment {
+        int start;
+        int length;
+        Segment(int start, int length) {
+            this.start = start;
+            this.length = length;
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> startPosition = new HashMap<>();
+        Map<Integer, Integer> startToLength = new HashMap<>();
+        Map<Integer, Integer> numberToSegments = new HashMap<>();
+
+        int[] nums = new int[]{0,2,3,1,0,1,1,3,4};
+
+        int prev = nums[0];
+        int start = 0;
+
+        // edge
+        startPosition.put(start, nums[0]);
+        startToLength.put(start, 1);
+        numberToSegments.put(nums[0], 1);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (prev == nums[i]) {
+                startToLength.merge(start, 1, Integer::sum);
+            } else {
+                start = i;
+                startToLength.put(start, 1);
+                startPosition.put(start, nums[i]);
+                numberToSegments.merge(nums[i], 1, Integer::sum);
+            }
+
+            prev = nums[i];
+        }
+
+        int[] startIndex = startToLength.keySet().stream().mapToInt(Number::intValue).toArray();
+        Arrays.sort(startIndex);
+
+        int prevStart = startIndex[0];
+        int maxLength = 0;
+        for (int i = 1; i < startIndex.length; i++) {
+            int nextStart = startToLength.get(prevStart) + prevStart + 1;
+            int currentLength = startToLength.get(startIndex[i]);
+            if (startToLength.containsKey(nextStart)
+                    && startPosition.get(prevStart).equals(nums[nextStart])
+                    && numberToSegments.get(nums[nextStart]) >= 3) {
+                maxLength = Math.max(startToLength.get(nextStart) + 1 + currentLength, maxLength);
+            } else {
+                if (numberToSegments.get(nums[startIndex[i]]) >= 2) {
+                    currentLength++;
+                }
+                maxLength = Math.max(currentLength, maxLength);
+            }
+
+            prevStart = startIndex[i];
+        }
+
+        System.out.println(maxLength);
     }
 }
