1f0f5a8216f78b3fe65bf72ba76dbcd46cdfe17a
fghpdf <quxiangxuanqxx@gmail.com>
2020-09-22 20:12:08 +0800

    feat: solved 129 add iterative

--- src/com/fghpdf/BinaryTreePreorderTraversal/Solution.java
+++ src/com/fghpdf/BinaryTreePreorderTraversal/Solution.java
@@ -3,7 +3,9 @@ package com.fghpdf.BinaryTreePreorderTraversal;
 import com.fghpdf.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author qxx
@@ -26,6 +28,33 @@ public class Solution {
 
         // right
         result.addAll(preorderTraversal(root.right));
+
+        return result;
+    }
+
+    public List<Integer> iterative(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+
+        // stock right node
+        Stack<TreeNode> rightNodes = new Stack<>();
+
+        // loop
+        while (root != null) {
+            result.add(root.val);
+            if (root.right != null) {
+                rightNodes.add(root.right);
+            }
+
+            root = root.left;
+
+            // leaf
+            if (root == null && !rightNodes.isEmpty()) {
+                root = rightNodes.pop();
+            }
+        }
 
         return result;
     }
