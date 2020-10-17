827ccba6da296b7b08b2e0e1a3c06ae5f4d34556
fghpdf <quxiangxuanqxx@gmail.com>
2020-10-17 18:26:02 +0800

    feat: solved 92

---
+++ src/com/fghpdf/ReverseLinkedListII/Solution.java
@@ -0,0 +1,57 @@
+package com.fghpdf.ReverseLinkedListII;
+
+import com.fghpdf.ListNode;
+
+/**
+ * @author qxx
+ * @date 10/17/2020
+ * @link https://leetcode.com/problems/reverse-linked-list-ii/
+ **/
+public class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        if (m == n) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0); // create a dummy node to mark the head of this list
+        dummy.next = head;
+        ListNode pre = dummy; // make a pointer pre as a marker for the node before reversing
+        for(int i = 0; i < m-1; i++) {
+            pre = pre.next;
+        }
+
+        ListNode start = pre.next; // a pointer to the beginning of a sub-list that will be reversed
+        ListNode then = start.next; // a pointer to a node that will be reversed
+
+        // 1 - 2 -3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
+        // dummy-> 1 -> 2 -> 3 -> 4 -> 5
+
+        for(int i=0; i < n-m; i++) {
+            start.next = then.next; // connect 2 -> 4
+            then.next = pre.next; // connect 3 -> 2
+            pre.next = then; // connect 1 -> 2
+            then = start.next; // move point to 4, start still is 2
+        }
+
+        // first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
+        // second reversing: dummy->1 - 4 - 3 - 2 - 5; pre = 1, start = 2, then = 5 (finish)
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        Solution sol = new Solution();
+        ListNode res = sol.reverseBetween(head, 2, 4);
+        res.printlist(res);
+    }
+
+}
