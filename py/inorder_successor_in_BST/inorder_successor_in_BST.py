a0b3e67b237f482314952d97dc4bd4af4fc2c0eb
fghpdf <fghpdf@outlook.com>
2021-11-27 10:59:23 +0800

    feat: 285. Inorder Successor in BST

---
+++ py/inorder_successor_in_BST/inorder_successor_in_BST.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
+        if not root or not p:
+            return None
+
+        if root.val <= p.val:
+            return self.inorderSuccessor(root.right, p)
+        else:
+            left = self.inorderSuccessor(root.left, p)
+            if left:
+              return left
+            else:
+              return root
