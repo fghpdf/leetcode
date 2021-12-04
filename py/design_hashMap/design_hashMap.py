f621e822adfae238682d7b291f16baa801c069cf
fghpdf <fghpdf@outlook.com>
2021-12-04 17:43:50 +0800

    feat: 706. Design HashMap

---
+++ py/design_hashMap/design_hashMap.py
@@ -0,0 +1,49 @@
+class ListNode:
+    def __init__(self, key, val):
+        self.pair = (key, val)
+        self.next = None
+
+class MyHashMap:
+
+    def __init__(self):
+        self.m = 1000
+        self.h = [None]*self.m
+
+    def put(self, key: int, value: int) -> None:
+        index = key % self.m
+        if self.h[index] == None:
+            self.h[index] = ListNode(key, value)
+        else:
+            cur = self.h[index]
+            while True:
+                if cur.pair[0] == key:
+                    cur.pair = (key, value) #update
+                    return
+                if cur.next == None: break
+                cur = cur.next
+            cur.next = ListNode(key, value)
+
+    def get(self, key: int) -> int:
+        index = key % self.m
+        cur = self.h[index]
+        while cur:
+            if cur.pair[0] == key:
+                return cur.pair[1]
+            else:
+                cur = cur.next
+        return -1
+
+    def remove(self, key: int) -> None:
+        index = key % self.m
+        cur = prev = self.h[index]
+        if not cur: return
+        if cur.pair[0] == key:
+            self.h[index] = cur.next
+        else:
+            cur = cur.next
+            while cur:
+                if cur.pair[0] == key:
+                    prev.next = cur.next
+                    break
+                else:
+                    cur, prev = cur.next, prev.next
