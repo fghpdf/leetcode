e6ffd900be8f95dc09152289b8653cc0f568f9c4
fghpdf <fghpdf@outlook.com>
2022-02-15 08:50:30 +0800

    feat: 652. Find Duplicate Subtrees

---
+++ py/find_duplicate_subtrees/find_duplicate_subtrees.py
@@ -0,0 +1,27 @@
+'''
+Author: fghpdf
+Date: 2022-02-15 08:48:26
+LastEditTime: 2022-02-15 08:48:26
+LastEditors: fghpdf
+'''
+import collections
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        def getid(root):
+            if root:
+                id = treeid[root.val, getid(root.left), getid(root.right)]
+                trees[id].append(root)
+                return id
+        trees = collections.defaultdict(list)
+        treeid = collections.defaultdict()
+        treeid.default_factory = treeid.__len__
+        getid(root)
+        return [roots[0] for roots in trees.values() if roots[1:]]
