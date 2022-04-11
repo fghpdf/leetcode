b12b6b2c3e791059f207a5d3d64580cf24c6d8b3
fghpdf <fghpdf@outlook.com>
2022-04-11 09:49:27 +0800

    feat: 1260. Shift 2D Grid

--- py/shift_2d_grid/shift_2d_grid.py
+++ py/shift_2d_grid/shift_2d_grid.py
@@ -20,6 +20,13 @@ class Solution:
         grid1[0][0] = grid[-1][-1]
         k -= 1
         return self.shiftGrid(grid1, k)
+
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        col, nums = len(grid[0]), sum(grid, [])
+        k = k % len(nums)
+        nums = nums[-k:] + nums[:-k]
+        return [nums[i:i+col] for i in range(0, len(nums), col)]
         
 
 class TestSolution(unittest.TestCase):
