77881870ea8ef5f663b4918dff2fa8be9032c9c4
fghpdf <fghpdf@outlook.com>
2022-04-11 09:48:45 +0800

    feat: 1260. Shift 2D Grid

---
+++ py/shift_2d_grid/shift_2d_grid.py
@@ -0,0 +1,33 @@
+from typing import List
+import unittest
+
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        if len(grid) == 0:
+          return []
+
+        if k == 0:
+          return grid
+
+        grid1 = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
+        for i in range(len(grid)):
+          for j in range(len(grid[i])):
+            if j == len(grid[i])-1 and i < len(grid)-1:
+              grid1[i+1][0] = grid[i][j]
+            elif j < len(grid[i]) -1:
+              grid1[i][j+1] = grid[i][j]
+        
+        grid1[0][0] = grid[-1][-1]
+        k -= 1
+        return self.shiftGrid(grid1, k)
+        
+
+class TestSolution(unittest.TestCase):
+    def testShiftGrid(self):
+        sol = Solution()
+        self.assertEqual(sol.shiftGrid(grid = [[1,2,3],[4,5,6],[7,8,9]], k = 1), [[9,1,2],[3,4,5],[6,7,8]])
+        self.assertEqual(sol.shiftGrid(grid = [[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]], k = 4), [[12,0,21,13],[3,8,1,9],[19,7,2,5],[4,6,11,10]])
+        self.assertEqual(sol.shiftGrid(grid = [[1,2,3],[4,5,6],[7,8,9]], k = 9), [[1,2,3],[4,5,6],[7,8,9]])
+
+if __name__ == '__main__':
+    unittest.main()
