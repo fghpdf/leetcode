27a3b782fb9e10c3af62594ca312a445de9c504d
fghpdf <fghpdf@outlook.com>
2022-04-27 09:22:41 +0800

    update: Smallest String With Swaps

--- py/smallest_string_with_swaps/smallest_string_with_swaps.py
+++ py/smallest_string_with_swaps/smallest_string_with_swaps.py
@@ -4,10 +4,13 @@ from typing import DefaultDict, List
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
         class UF:
-            def __init__(self, n): self.p = list(range(n))
-            def union(self, x, y): self.p[self.find(x)] = self.find(y)
+            def __init__(self, n):
+                self.p = list(range(n))
+            def union(self, x, y):
+                self.p[self.find(x)] = self.find(y)
             def find(self, x):
-                if x != self.p[x]: self.p[x] = self.find(self.p[x])
+                if x != self.p[x]:
+                    self.p[x] = self.find(self.p[x])
                 return self.p[x]
         uf, res, m = UF(len(s)), [], DefaultDict(list)
         for x,y in pairs: 
